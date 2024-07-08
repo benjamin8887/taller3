2801ecf98714ca6c25b65a6f7b1a60a13d63723c
benjamin8887 <benjaminfitzgerald888@gmail.com>
2024-07-08 16:29:28 -0400

    avance2

--- Assets/Taller 1/EnemyItem.cs
+++ Assets/Taller 1/EnemyItem.cs
@@ -4,12 +4,11 @@ public class EnemyItem : MonoBehaviour
 {
     public GameObject itemDrop; // Objeto que el enemigo dejará al morir
 
-    private void Die()
+    private void OnDestroy()
     {
         // Instanciar el objeto que se va a dejar caer
-        Instantiate(itemDrop, transform.position, Quaternion.identity);
+        GameObject go = Instantiate(itemDrop, transform.position, Quaternion.identity);
+        go.name = itemDrop.name;
 
-        // Destruir al enemigo
-        Destroy(gameObject);
     }
 }
